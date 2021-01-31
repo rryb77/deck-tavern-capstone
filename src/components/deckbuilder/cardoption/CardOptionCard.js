78468390c5174760d0aaf40cd2322115613c30d3
Ryan Youngblood <rryb77@gmail.com>
2021-01-30 19:44:35 -0600

    card id's can now be pulled when clicked!

--- src/components/deckbuilder/cardoption/CardOptionCard.js
+++ src/components/deckbuilder/cardoption/CardOptionCard.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 // import { useHistory } from 'react-router-dom';
 
 export const CardOptionCard = ({card}) => {
     
     // const history = useHistory()
     let [countCards, setCountCards] = useState(0)
-    
-    const cardOptionChosen = () => {
+
+    const cardOptionChosen = (card) => {
         console.log('Card clicked')
-        
+        // console.log(event.target.id)
+        console.log(card)
+
         if (countCards < 2) {
             const newCardCount = ++countCards
             setCountCards(newCardCount)
@@ -20,7 +22,7 @@ export const CardOptionCard = ({card}) => {
         
         <section className="cardViewerOptions">
               <div className="cardImage">
-                <img src={`https://art.hearthstonejson.com/v1/render/latest/enUS/256x/${card.id}.png`} className="card_image" id={`card--${card?.dbfId}`} onClick={event => cardOptionChosen()} alt={`${card?.name}`}/>
+                <img src={`https://art.hearthstonejson.com/v1/render/latest/enUS/256x/${card.id}.png`} className="card_image" id={`${card?.dbfId}`} onClick={event => cardOptionChosen(card.dbfId)} alt={`${card?.name}`}/>
               </div>
               <div className="cardCount">{countCards} /2</div>
         </section>
